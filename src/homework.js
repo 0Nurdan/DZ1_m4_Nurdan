e1b797a17cc762d7c40d92a084d105fc03e5e039
0Nurdan <121168145+0Nurdan@users.noreply.github.com>
2022-12-21 17:50:47 +0600

    Add files via upload

---
+++ src/homework.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+
+function MyInfo() {
+    return (
+        <div>
+            <h1>Нурдан</h1>
+            <p>Я учусь в 10 классе. Мне 16 лет. У меня есть двойняшка Айдана. Я учусь в школе "Bright"</p>
+            <h3>Места которые я хочу посетить:</h3>
+            <ul>
+                <li>США</li>
+                <li>Канада</li>
+                <li>Франция</li>
+            </ul>
+        </div>
+    );
+}
+export default MyInfo;
